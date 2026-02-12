192f987cc3592344ca3a07ad7edeb2890e6e6046
muhallilahnaf <muhallilahnaf@gmail.com>
2026-02-12 22:38:48 +0600

    collect and process from dt

--- collect-data.py
+++ collect-data.py
@@ -1,6 +1,83 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import re
 import pandas as pd
+from time import sleep
 
-df = pd.read_csv('data/votes_raw.csv')
+
+dfraw = pd.read_csv('data/votes_raw.csv')
 
 # collect data from api
+dfd = pd.read_csv('data/dt_seats.csv')
+rstore = []
+count = 5
+for i, row in dfd.iterrows():
+    res = requests.get(f'https://election.dhakatribune.com/get-candidate/{row.seat_no}')
+    # print(row.seat_name, res.status_code)
+    if res.status_code == 200:
+        data = res.json()
+        for candidate in data:
+            rstore.append(candidate)
+    count = count - 1
+    if count == 0:
+        # print('sleeping')
+        sleep(2)
+        count = 5
+
+dfr = pd.DataFrame(rstore)
+
+# map party name
+party_mapping = {
+    '38': 'BNP',
+    '36': 'Jamaat',
+    '66': 'AB Party',
+    '37': 'Islami Andolan Bangladesh',
+    '5': 'JaPa',
+    '47': 'CPB',
+    '90': 'Ganosamhati Andolon (Saki)',
+    '63': 'NCP',
+    '50': 'Bangladesh Khelafat Majlish',
+    '115': 'Gano Forum',
+    '99': 'Jamiat Ulema-e-Islam Bangladesh',
+    '45': 'Khelafat Majlish',
+    '65': 'BJP'
+}
+
+def map_party(x):
+    x = str(x)
+    if x in party_mapping.keys():
+        return party_mapping[x]
+    return None
+
+dfr['party'] = dfr.party_id.apply(map_party)
+
+# drop less important parties
+dfr = dfr.dropna(subset=['party'])
+
+# merge to get seat name
+df = pd.merge(dfr, dfd, how='left', left_on='seat_id', right_on='seat_no')
+df = df[['seat_name', 'party', 'name', 'vote', 'center', 'division_id', 'total_center']]
+
+# map seat name
+seat_mapping = {
+    'Chapainawabganj': 'Chapai Nawabganj',
+    'Jhalakathi': 'Jhalokati'
+}
+
+def map_seat(x):
+    name, number = x.split('-')
+    if name in seat_mapping.keys():
+        return seat_mapping[name] + '-' + number
+    return x
+
+df['seat_name'] = df.seat_name.apply(map_seat)
+
+# add timestamp and source
+df['time'] = pd.to_datetime('now', utc=True)
+df['source'] = 'dt'
+
 # append to votes_raw.csv
+dfnew = pd.concat([dfraw, df], ignore_index=True)
+
+# save
+dfnew.to_csv('data/votes_raw.csv', index=False)
--- process-data.py
+++ process-data.py
@@ -4,32 +4,36 @@ import pandas as pd
 # get vote pc
 def get_vote_pc(row):
     try:
-        return round(row.votes / row.voters * 100, 2)
+        return round(row.vote / row.voters * 100, 2)
     except:
         return 0
 
 
 # read data
-dfv = pd.read_csv('/data/votes_raw.csv')
-dfs = pd.read_csv('data/seat_voters_2026.csv')
+dfv = pd.read_csv('data/votes_raw.csv') # seat_name,party,name,vote,center,division_id,total_center,time,source
+dfs = pd.read_csv('data/seat_voters_2026.csv') # seat,voters,male_voters,female_voters,thirdgender_voters
 
 
 # get seat-wise and party-wise latest vote count
-dfvc = dfv.groupby(['seat', 'party']).agg({'time': 'max', 'votes': 'first'}).reset_index()
-dfvpc = pd.merge(dfvc, dfs, how='left', on='seat')
+idx = dfv.groupby(['seat_name', 'party'])['time'].idxmax()
+dfvc = dfv.loc[idx]
+dfvpc = pd.merge(dfvc, dfs, how='left', left_on='seat_name', right_on='seat')
 dfvpc['vote_pc'] = dfvpc.apply(get_vote_pc, axis=1)
+dfvpc = dfvpc.drop(columns=['seat'])
 
 # save seat-wise and party-wise data
 dfvpc.to_csv('data/seat_votes.csv', index=False)
 
 
 # get party-wise summary
-dfpc = dfvc.groupby('party').agg({'time': 'max', 'votes': 'sum', 'voters': 'sum'}).reset_index()
+dfpc = dfvpc.groupby('party').agg({'time': 'max', 'vote': 'sum', 'voters': 'sum'}).reset_index()
 dfpc['vote_pc'] = dfpc.apply(get_vote_pc, axis=1)
 
 # get seat-wise party summary
-dfps = dfvc.groupby('seat').agg({'time': 'max', 'votes': 'max', 'party': 'first'}).reset_index()
-dfps = dfps.groupby('party')['seat'].count().reset_index()
+latest_time = dfvpc.groupby('seat_name')['time'].max().reset_index()
+df_latest = dfvpc.merge(latest_time, on=['seat_name', 'time'])
+dfps = df_latest.sort_values(['seat_name', 'vote'], ascending=[True, False]).drop_duplicates('seat_name')
+dfps = dfps.groupby('party')['seat_name'].count().reset_index()
 dfp = pd.merge(dfpc, dfps, how='left', on='party')
 
 # save party-wise data
